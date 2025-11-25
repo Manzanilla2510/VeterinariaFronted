9b2544cad5e910b3cad4647ce084488428bd7432
Manzanilla <aramayodaniel424@gmail.com>
2025-11-24 23:26:15 -0400

    mm33

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { PlantillaAnimales } from "./Componentes/Plantillas/PlantillaAnimal";
-import { PlantillaVeterinarios } from "./Componentes/Plantillas/PlantillaVeterinario";
+import { PlantillaAnimales } from "./Componentes/Paginas/PaginaAnimal";
+import { PlantillaVeterinarios } from "./Componentes/Paginas/PaginaVeterinario";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { PlantillaInventario } from "./Componentes/Plantillas/PlantillaInventario";
-import { PlantillaRevisiones } from "./Componentes/Plantillas/PlantillaRevison";
+import { PlantillaInventario } from "./Componentes/Paginas/PaginaInventario";
+import { PlantillaRevisiones } from "./Componentes/Paginas/PaginaRevison";
 import { Navbar } from "./Componentes/Organismos/Header";
 import { Footer } from "./Componentes/Organismos/Footer";
-import { PlantillaAlimentacion } from "./Componentes/Plantillas/PlantillaAlimentacion";
-import { PlantillaSolicitudPersonal } from "./Componentes/Plantillas/PlantillaSolicitudPersonal";
+import { PlantillaAlimentacion } from "./Componentes/Paginas/PaginaAlimentacion";
+import { PlantillaSolicitudPersonal } from "./Componentes/Paginas/PaginaSolicitudPersonal";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
